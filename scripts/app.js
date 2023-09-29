5b6d7139b947bf503c61bee4e8658cbdd49f2a5c
Felipe Amorim <felipeamorim.go@gmail.com>
2023-09-29 01:22:46 -0300

    Commit

--- scripts/app.js
+++ scripts/app.js
@@ -10,20 +10,24 @@ const botaoUmModal = document.querySelector(".modal__principalbtn")
 const botaoDoisModal = document.querySelector(".modal__secundariobtn")
 const vsCPUBtn = document.querySelector(".newgame__vscpu")
 const vsPlayerBtn = document.querySelector(".newgame__vsplayer")
+const firstX = document.querySelector(".painel__img:first-of-type")
+const firstO = document.querySelector(".painel__img:last-of-type")
 let casasLivres = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 let placar = [0, 0, 0]
-let jogadorVez = 0
+let placarJogadorUm = placar[0]
+let placarJogadorDois = placar[2]
+let placarEmpates = placar[1]
+let jogadorVez = 1
 let tipoJogador = 0
-let ganhador = false
+let firstIsX = false
 let contadorJogadas = 0
+let ganhador = false
 let tabuleiro = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9]
 ]
-let placarJogadorUm = placar[0]
-let placarJogadorDois = placar[2]
-let placarEmpates = placar[1]
+
 
 const jogadorHandle = event => {
   if (jogadorVez === 0) {
@@ -33,29 +37,29 @@ const jogadorHandle = event => {
 
     if (estaSelecional) {
       contadorJogadas++
-      prencherTabuleiro(posicaoCasaSelecionada, casaClicada,"./assets/icon-x.svg", "X")
-      verificaGanhador("X")
-      jogadorUmVencedor()
+      prencherTabuleiro(posicaoCasaSelecionada, casaClicada, firstIsX ? "./assets/icon-x.svg" : "./assets/icon-o.svg", firstIsX ? "X" : "O")
+      verificaGanhador(firstIsX ? "X" : "O")
+      jogadorUmVencedor(firstIsX ? "X" : "O")
     } else {
       console.log("esta selecionada")
     }
-  }else if (tipoJogador === 1){
+  } else if (tipoJogador === 1) {
     const casaClicada = event.currentTarget
     const posicaoCasaSelecionada = Number(casaClicada.getAttribute("casa"))
     const estaSelecional = casasLivres.includes(posicaoCasaSelecionada)
 
     if (estaSelecional) {
       contadorJogadas++
-      prencherTabuleiro(posicaoCasaSelecionada, casaClicada, "./assets/icon-o.svg", "O")
-      verificaGanhador("O")
-      jogadorDoisVencedor()
+      prencherTabuleiro(posicaoCasaSelecionada, casaClicada, firstIsX ? "./assets/icon-o.svg" : "./assets/icon-x.svg", firstIsX ? "O" : "X")
+      verificaGanhador(firstIsX ? "O" : "X")
+      jogadorDoisVencedor(firstIsX ? "O" : "X")
     } else {
       console.log("esta selecionada")
     }
   }
 }
 
-function prencherTabuleiro(posicao, casaClicada, src , player) {
+function prencherTabuleiro(posicao, casaClicada, src, player) {
   const indexCasa = casasLivres.indexOf(posicao)
   const indexTabuleiro = Number(casaClicada.getAttribute("casa")) - 1
 
@@ -72,155 +76,6 @@ function marcaNoTabuleiro(posicao, jogador) {
       }
     }
   }
-}
-
-function alterarContador(elemento, valor) {
-  elemento.textContent = valor
-}
-
-function jogadorUmVencedor() {
-
-  if (ganhador == true) {
-    placarJogadorUm++
-    modal.innerHTML = `<span class="modal__subtitle font-principal-xs cor-7">YOU WON!</span>
-    <h1 class="modal__title font-principal-l  cor-7"><img src="./assets/icon-x.svg" class="icon-jogador">TAKES THE ROUND</h1>
-    <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="voltarMenu()">QUIT</button>
-    <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">NEXT ROUND</button>
-  `
-  modalJogo.classList.toggle("disablemodal")
-    
-    setTimeout(() => {
-      alterarContador(contadoresPlacar[0], placarJogadorUm)
-      limparTabuleiro()
-    }, 500)
-  } else {
-    jogadorVez++
-
-    if (contadorJogadas === 9) {
-      placarEmpates++
-      modal.innerHTML = `
-      <h1 class="modal__title font-principal-l  cor-7">ROUND TIED</h1>
-      <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="voltarMenu()">QUIT</button>
-      <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">NEXT ROUND</button>
-    `
-    modalJogo.classList.toggle("disablemodal")
-      setTimeout(() => {
-        alterarContador(contadoresPlacar[1], placarEmpates)
-        limparTabuleiro()
-      }, 500)
-
-      return
-    }
-    mudarVezHTML("./assets/jogador-vez-o.svg")
-   
-    if(!(tipoJogador === 1)){
-      setTimeout(() => {
-        jogadaBot()
-      }, 1000)
-    }
-    }
-}
-
-function jogadorDoisVencedor() {
-
-  if (ganhador == true) {
-    placarJogadorDois++
-    modal.innerHTML = `<span class="modal__subtitle font-principal-xs cor-7">PLAYER 2 WINS!</span>
-    <h1 class="modal__title font-principal-l  cor-7"><img src="./assets/icon-o.svg" class="icon-jogador">TAKES THE ROUND</h1>
-    <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="voltarMenu()">QUIT</button>
-    <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">NEXT ROUND</button>
-  `
-  modalJogo.classList.toggle("disablemodal")
-    
-    setTimeout(() => {
-      alterarContador(contadoresPlacar[2], placarJogadorDois)
-      limparTabuleiro()
-    }, 500)
-  } else {
-    jogadorVez--
-
-    // if (contadorJogadas === 9) {
-    //   placarEmpates++
-    //   modal.innerHTML = `
-    //   <h1 class="modal__title font-principal-l  cor-7">ROUND TIED</h1>
-    //   <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="proximoJogo()">QUIT</button>
-    //   <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">NEXT ROUND</button>
-    // `
-    // modalJogo.classList.toggle("disablemodal")
-    //   setTimeout(() => {
-    //     alterarContador(contadoresPlacar[1], placarEmpates)
-    //     limparTabuleiro()
-    //   }, 500)
-
-    //   return
-    // }
-    mudarVezHTML("./assets/jogador-vez-x.svg")
-    }
-}
-
-function cpuVenceu() {
-  if (ganhador == true) {
-    placarJogadorDois++
-    modal.innerHTML = `<span class="modal__subtitle font-principal-xs cor-7">OH NO, YOU LOST…</span>
-    <h1 class="modal__title font-principal-l  cor-3"><img src="./assets/icon-o.svg" class="icon-jogador">TAKES THE ROUND</h1>
-    <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="voltarMenu()">QUIT</button>
-    <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">NEXT ROUND</button>
-  `
-  modalJogo.classList.toggle("disablemodal")
-    setTimeout(() => {
-      alterarContador(contadoresPlacar[2], placarJogadorDois)
-      limparTabuleiro()
-    }, 500)
-
-  } else {
-    jogadorVez--
-    mudarVezHTML("./assets/jogador-vez-x.svg")
-  }
-}
-
-function proximoJogo(){
-  limparTabuleiro()
-  modalJogo.classList.toggle("disablemodal")
-}
-
-function voltarMenu(){
-  limparTabuleiro()
-  tipoJogador = 0
-  placarJogadorUm = 0
-  placarJogadorDois = 0
-  placarEmpates = 0
-  alterarContador(contadoresPlacar[0], placarJogadorUm)
-  alterarContador(contadoresPlacar[2], placarJogadorDois)
-  alterarContador(contadoresPlacar[1], placarEmpates)
-  modalJogo.classList.toggle("disablemodal")
-  game.classList.add("hidden")
-  menu.classList.remove("hidden")
-}
-
-function voltarAoJogo(){
-  modalJogo.classList.toggle("disablemodal")
-}
-
-function mudarVezHTML(src) {
-  turnPlayer.setAttribute("src", src)
-}
-
-function jogadaBot() {
-  const indexAleatorio = Math.ceil(Math.random() * casasLivres.length) - 1
-  const casaEscolhida = casasLivres[indexAleatorio]
-  const indexJogadaCasa = casasLivres.indexOf(casaEscolhida)
-  const indexCasaSelecionada = casaEscolhida - 1
-
-  casasLivres.splice(indexJogadaCasa, 1)
-  marcaNoTabuleiro(casaEscolhida, "O")
-  marcaNoHTML(indexCasaSelecionada, './assets/icon-o.svg')
-  verificaGanhador("O")
-  cpuVenceu()
-  contadorJogadas++
-}
-
-function controleJogadas(index) {
-  casasLivres.splice(index, 1)
 }
 
 function marcaNoHTML(indexJogada, src) {
@@ -271,9 +126,186 @@ function verificarGanhadorDiagonal(jogador) {
   }
 }
 
+function jogadorUmVencedor(tipo) {
+
+  const tipoJogadorIs = firstIsX === true ? "O" : "X"
+
+  if (ganhador == true) {
+    placarJogadorUm++
+    modal.innerHTML = `<span class="modal__subtitle font-principal-xs cor-7">YOU WON!</span>
+    <h1 class="modal__title font-principal-l  ${tipoJogadorIs === "X" ? "cor-3" : "cor-2"}"><img src="./assets/icon-${tipo.toLowerCase()}.svg" class="icon-jogador">TAKES THE ROUND</h1>
+    <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="voltarMenu()">QUIT</button>
+    <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">NEXT ROUND</button>
+  `
+    modalJogo.classList.toggle("disablemodal")
+    if(tipo === "X"){
+      alterarContador(contadoresPlacar[0], placarJogadorUm)
+    }else{
+      alterarContador(contadoresPlacar[2], placarJogadorUm)
+    }
+    setTimeout(() => {
+      limparTabuleiro()
+    }, 1000)
+  } else {
+    jogadorVez++
+
+    if (contadorJogadas === 9) {
+      placarEmpates++
+      modal.innerHTML = `
+      <h1 class="modal__title font-principal-l  cor-7">ROUND TIED</h1>
+      <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="voltarMenu()">QUIT</button>
+      <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">NEXT ROUND</button>
+    `
+      modalJogo.classList.toggle("disablemodal")
+      alterarContador(contadoresPlacar[1], placarEmpates)
+      setTimeout(() => {
+        limparTabuleiro()
+      }, 1000)
+
+      return
+    }
+    mudarVezHTML(`./assets/jogador-vez-${tipoJogadorIs.toLowerCase()}.svg`)
+
+    if (!(tipoJogador === 1)) {
+      setTimeout(() => {
+        jogadaBot(tipoJogadorIs)
+      }, 1000)
+    }
+  }
+}
+
+function jogadorDoisVencedor(tipo) {
+  const tipoJogadorIs = firstIsX === true ? "X" : "O"
+  if (ganhador == true) {
+    placarJogadorDois++
+    modal.innerHTML = `<span class="modal__subtitle font-principal-xs cor-7">PLAYER 2 WINS!</span>
+    <h1 class="modal__title font-principal-l ${tipoJogadorIs === "X" ? "cor-3" : "cor-2"}"><img src="./assets/icon-${tipo.toLowerCase()}.svg" class="icon-jogador">TAKES THE ROUND</h1>
+    <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="voltarMenu()">QUIT</button>
+    <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">NEXT ROUND</button>
+  `
+    modalJogo.classList.toggle("disablemodal")
+    alterarContador(contadoresPlacar[2], placarJogadorDois)
+    setTimeout(() => {
+      limparTabuleiro()
+    }, 1000)
+  } else {
+    jogadorVez--
+
+    if (contadorJogadas === 9) {
+      placarEmpates++
+      modal.innerHTML = `
+      <h1 class="modal__title font-principal-l  cor-7">ROUND TIED</h1>
+      <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="voltarMenu()">QUIT</button>
+      <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">NEXT ROUND</button>
+    `
+      modalJogo.classList.toggle("disablemodal")
+      alterarContador(contadoresPlacar[1], placarEmpates)
+      setTimeout(() => {
+        limparTabuleiro()
+      }, 1000)
+
+      return
+    }
+    mudarVezHTML(`./assets/jogador-vez-${tipoJogadorIs.toLowerCase()}.svg`)
+  }
+}
+
+function cpuVenceu(tipo) {
+  const tipoJogador = tipo === "X" ? "O" : "X"
+  if (ganhador == true) {
+    placarJogadorDois++
+    modal.innerHTML = `<span class="modal__subtitle font-principal-xs cor-7">OH NO, YOU LOST…</span>
+    <h1 class="modal__title font-principal-l  ${tipo === "X"? "cor-2" : "cor-3"}"><img src="./assets/icon-${tipo.toLowerCase()}.svg" class="icon-jogador">TAKES THE ROUND</h1>
+    <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="voltarMenu()">QUIT</button>
+    <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">NEXT ROUND</button>
+  `
+    modalJogo.classList.toggle("disablemodal")
+    if(tipo === "X"){
+      alterarContador(contadoresPlacar[0], placarJogadorDois)
+    }else{
+      alterarContador(contadoresPlacar[2], placarJogadorDois)
+    }
+    
+    setTimeout(() => {
+      limparTabuleiro()
+    }, 1000)
+
+  } else {
+    jogadorVez--
+    if (contadorJogadas === 9) {
+      placarEmpates++
+      modal.innerHTML = `
+      <h1 class="modal__title font-principal-l  cor-7">ROUND TIED</h1>
+      <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="voltarMenu()">QUIT</button>
+      <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">NEXT ROUND</button>
+    `
+      modalJogo.classList.toggle("disablemodal")
+      alterarContador(contadoresPlacar[1], placarEmpates)
+      setTimeout(() => {
+        limparTabuleiro()
+      }, 1000)
+
+      return
+    }
+    mudarVezHTML(`./assets/jogador-vez-${tipoJogador.toLowerCase()}.svg`)
+  }
+}
+
+function jogadaBot(tipo) {
+  const indexAleatorio = Math.ceil(Math.random() * casasLivres.length) - 1
+  const casaEscolhida = casasLivres[indexAleatorio]
+  const indexJogadaCasa = casasLivres.indexOf(casaEscolhida)
+  const indexCasaSelecionada = casaEscolhida - 1
+
+  casasLivres.splice(indexJogadaCasa, 1)
+  contadorJogadas++
+  marcaNoTabuleiro(casaEscolhida, tipo)
+  marcaNoHTML(indexCasaSelecionada, `./assets/icon-${tipo.toLowerCase()}.svg`)
+  verificaGanhador(tipo)
+  cpuVenceu(tipo)
+
+}
+
+function alterarContador(elemento, valor) {
+  elemento.textContent = valor
+}
+
+function mudarVezHTML(src) {
+  turnPlayer.setAttribute("src", src)
+}
+
+function proximoJogo() {
+  limparTabuleiro()
+  botJogaPrimeiro()
+  modalJogo.classList.toggle("disablemodal")
+}
+
+function voltarMenu() {
+  tipoJogador = 0
+  limparTabuleiro()
+  limparContadores()
+  modalJogo.classList.toggle("disablemodal")
+  game.classList.add("hidden")
+  menu.classList.remove("hidden")
+}
+
+function limparContadores() {
+  placarJogadorUm = 0
+  placarJogadorDois = 0
+  placarEmpates = 0
+  alterarContador(contadoresPlacar[0], placarJogadorUm)
+  alterarContador(contadoresPlacar[2], placarJogadorDois)
+  alterarContador(contadoresPlacar[1], placarEmpates)
+}
+
+function voltarAoJogo() {
+  modalJogo.classList.toggle("disablemodal")
+}
+
 function limparTabuleiro() {
   casasLivres = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  jogadorVez = 0
+  jogadorVez = firstIsX ? 0 : 1
+
   contadorJogadas = 0
   tabuleiro = [
     [1, 2, 3],
@@ -290,24 +322,30 @@ function limparTabuleiro() {
   })
 }
 
-function montarHTMLPagina() {
-
-  
+function botJogaPrimeiro() {
+  if (!firstIsX && tipoJogador == 0) {
+    const labelContadores = document.querySelectorAll(".contador__player")
+    labelContadores[0].innerText = "X (CPU)"
+    labelContadores[2].innerText = "O (YOU)"
+    jogadorVez = 1
+    setTimeout(() => {
+      jogadaBot("X")
+    }, 500)
+  }
 }
 
 vsCPUBtn.addEventListener("click", () => {
   game.classList.remove("hidden")
   menu.classList.add("hidden")
- })
 
- vsPlayerBtn.addEventListener("click", () => {
+  botJogaPrimeiro()
+})
+
+vsPlayerBtn.addEventListener("click", () => {
   tipoJogador = 1
   game.classList.remove("hidden")
   menu.classList.add("hidden")
- })
-
-
-
+})
 
 casasTabuleiro.forEach((casa, index) => {
   casa.setAttribute("casa", index + 1)
@@ -320,6 +358,31 @@ botaoRestart.addEventListener("click", event => {
       <button class="modal__principalbtn font-principal-xs cor-5 btn-secondary-minor" onClick="voltarAoJogo()">NO, CANCEL</button>
       <button class="modal__secundariobtn font-principal-xs cor-5 btn-primary-minor" onClick="proximoJogo()">YES, RESTART</button>
     `
-    modalJogo.classList.toggle("disablemodal")
+  modalJogo.classList.toggle("disablemodal")
 })
 
+firstX.addEventListener("click", event => {
+  firstIsX = true
+  jogadorVez = 0
+  const iconPlayerX = firstX.querySelector("img")
+  const iconPlayerO = firstO.querySelector("img")
+  iconPlayerX.setAttribute("src", "./assets/icon-x-menu-blue.svg")
+  firstO.classList.remove("ativo")
+  iconPlayerO.setAttribute("src", "./assets/icon-o-menu-blue.png")
+  event.currentTarget.classList.add("ativo")
+})
+
+firstO.addEventListener("click", event => {
+  jogadorPadrao(event)
+})
+
+function jogadorPadrao(event) {
+  firstIsX = false
+  const iconPlayerX = firstX.querySelector("img")
+  const iconPlayerO = firstO.querySelector("img")
+  iconPlayerX.setAttribute("src", "./assets/icon-x-menu.svg")
+  firstX.classList.remove("ativo")
+  iconPlayerO.setAttribute("src", "./assets/icon-o-menu.png")
+  event.currentTarget.classList.add("ativo")
+  jogadorVez++
+}
